ef65e52f6a2d2a37bf552b87b154aea17d6be8e9
Andres Carrillo <andresjch2804@gmail.com>
2018-10-01 17:31:40 -0500

    Integracion inicial de login con passport

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,52 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redirect', function () {
+    $query = http_build_query([
+        'client_id'     => '3',
+        'redirect_uri'  => 'http://exameneducacionmedia.test/home',
+        'response_type' => 'code',
+        'scope'         => '',
+    ]);
+
+    return redirect('http://jarvis.test/oauth/authorize?' . $query);
+});
+
+Route::get('/home', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://jarvis.test/oauth/token', [
+        'form_params' => [
+            'grant_type'    => 'authorization_code',
+            'client_id'     => '3',
+            'client_secret' => 'GiDCqoJw2Nw6RkWs9NPjCEe7THASGs0ZhIEPQYFG',
+            'redirect_uri'  => 'http://exameneducacionmedia.test/home',
+            'code'          => $request->code,
+        ],
+    ]);
+
+    return json_decode((string)$response->getBody(), true);
+});
+
+Route::get('/password', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://jarvis.test/oauth/token', [
+        'form_params' => [
+            'grant_type'    => 'password',
+            'client_id'     => '3',
+            'client_secret' => 'GiDCqoJw2Nw6RkWs9NPjCEe7THASGs0ZhIEPQYFG',
+            'username'      => 'mwhite',
+            'password'      => 'secret',
+            'code'          => $request->code,
+        ],
+    ]);
+
+    return json_decode((string)$response->getBody(), true);
 });
