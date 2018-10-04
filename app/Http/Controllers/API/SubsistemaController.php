ef869552ae0bbae7cb94d74345eee5c7c222e56a
Andres Carrillo <andresjch2804@gmail.com>
2018-10-04 10:27:41 -0500

    Agrega endpoint para consultar un subsistema

---
+++ app/Http/Controllers/API/SubsistemaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ExamenEducacionMedia\Http\Controllers\API;
+
+use ExamenEducacionMedia\Models\Subsistema;
+use ExamenEducacionMedia\Traits\ResponseTrait;
+use Illuminate\Http\Request;
+use ExamenEducacionMedia\Http\Controllers\Controller;
+
+class SubsistemaController extends Controller
+{
+    use ResponseTrait;
+
+    public function show(Subsistema $subsistema)
+    {
+        $subsistema->loadMissing('planteles', 'planteles.responsable', 'planteles.domicilio', 'especialidades');
+
+        return $this->respondWithArray(compact('subsistema'));
+    }
+}
