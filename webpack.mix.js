61a3d6dccbaec060a21dc82eef381a1c1937fd80
Andres Carrillo <andresjch2804@gmail.com>
2018-10-07 16:05:57 -0500

    Agrega componentes de vuetify

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.sass('resources/sass/adminlte.scss', 'public/css')
-    .sass('resources/sass/base.scss', 'public/css');
+    .sass('resources/sass/base.scss', 'public/css')
+    .sass('resources/sass/deamonite.scss', 'public/css');
 
 mix.js('resources/js/adminlte.js', 'public/js')
     .js('resources/js/app.js', 'public/js')
-    .js('resources/js/subsistemas/app.js', 'public/js/subsistemas');
+    .js('resources/js/subsistemas/app.js', 'public/js/subsistemas')
+    .js('resources/js/deamonite.js', 'public/js');
 
 if (mix.inProduction()) {
     mix.version();
