c504e260530addc0e0335f4bef900a0fd61d05e8
Andres Carrillo <andresjch2804@gmail.com>
2018-10-08 10:06:17 -0500

    Agrega componente para reusar tablas

---
+++ resources/js/components/TableComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th v-for="{text, value, align, width, index} in headers" :key="index" :class="'text-'+align" :width="width">{{ text }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.key">
+                <slot name="items" :item="item">
+                    <td v-for="{text, value, index} in headers" :key="index">
+                        {{ item[value] }}
+                    </td>
+                </slot>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        props: ['items', 'headers']
+    }
+</script>
+
+<style scoped>
+
+</style>
