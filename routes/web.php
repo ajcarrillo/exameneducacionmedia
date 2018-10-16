dc2c38d937e84f47734a675234d66eb053e6bce3
Andres Carrillo <andresjch2804@gmail.com>
2018-10-16 13:26:35 -0500

    Agrega pruebas para conectar con jarvis

--- routes/web.php
+++ routes/web.php
@@ -51,16 +51,17 @@ Route::middleware([ 'auth', 'role:subsistema', 'hasSubsistema' ])
             ->name('spa.subsistemas');
     });
 
-Route::get('/redirect', function () {
+/* Login con jarvis*/
+Route::get('/login-seq', function () {
     $query = http_build_query([
-        'client_id'     => '3',
+        'client_id'     => env('JARVIS_CLIENT_ID'),
         'redirect_uri'  => 'http://exameneducacionmedia.test/home',
         'response_type' => 'code',
         'scope'         => '',
     ]);
 
     return redirect('http://jarvis.test/oauth/authorize?' . $query);
-});
+})->name('login.seq');
 
 Route::get('/home', function (Request $request) {
     $http = new GuzzleHttp\Client;
@@ -68,8 +69,8 @@ Route::get('/home', function (Request $request) {
     $response = $http->post('http://jarvis.test/oauth/token', [
         'form_params' => [
             'grant_type'    => 'authorization_code',
-            'client_id'     => '3',
-            'client_secret' => 'GiDCqoJw2Nw6RkWs9NPjCEe7THASGs0ZhIEPQYFG',
+            'client_id'     => env('JARVIS_CLIENT_ID'),
+            'client_secret' => env('JARVIS_SECRET'),
             'redirect_uri'  => 'http://exameneducacionmedia.test/home',
             'code'          => $request->code,
         ],
@@ -84,20 +85,23 @@ Route::get('/password', function (Request $request) {
     $response = $http->post('http://jarvis.test/oauth/token', [
         'form_params' => [
             'grant_type'    => 'password',
-            'client_id'     => '3',
-            'client_secret' => 'GiDCqoJw2Nw6RkWs9NPjCEe7THASGs0ZhIEPQYFG',
-            'username'      => 'mwhite',
+            'client_id'     => env('JARVIS_CLIENT_ID'),
+            'client_secret' => env('JARVIS_SECRET'),
+            'username'      => 'ajcarrillo',
             'password'      => 'secret',
             'code'          => $request->code,
         ],
     ]);
 
+    //session()->put('token', json_decode((string) $response->getBody(), true));
+
     return json_decode((string)$response->getBody(), true);
 });
+/*Login con jarvis*/
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/vuetify', function () {
     return view('vuetify');
