a048f1ee26b96c8d008e68d0eec0d016e67f1c14
Andres Carrillo <andresjch2804@gmail.com>
2018-12-06 23:16:43 -0500

    Mejora interfaz para buscar centros de trabajo

---
+++ resources/js/administracion/planteles/components/RowItemComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="plantel-row">
+        <button class="btn btn-link btn-block card-btn p-3">
+            <span class="row">
+                <span class="col-12 col-md-6">{{ nombreCompleto }}</span>
+                <span class="col-12 col-md-2">{{ subsistema }}</span>
+                <span class="col-12 col-md-4">{{ munLoc }}</span>
+            </span>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RowItem",
+        props: ['plantel'],
+        computed: {
+            nombreCompleto() {
+                return `${this.plantel.clave} ${this.plantel.nombre}`
+            },
+            munLoc() {
+                return `${this.plantel.inmueble.municipio.NOM_MUN}/${this.plantel.inmueble.localidad.NOM_LOC}`;
+            },
+            subsistema() {
+                return this.plantel.subsistema == undefined ? 'SIN SUBSISTEMA' : this.plantel.subsistema.referencia;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .card-btn {
+        color: #1e2022;
+        text-align: left;
+        white-space: inherit;
+        font-size: 0.9375rem;
+    }
+
+    .card-btn:not(:disabled):not(.disabled).active, .card-btn:not(:disabled):not(.disabled):active {
+        -webkit-box-shadow: 0 0 0 0 transparent;
+        box-shadow: 0 0 0 0 transparent;
+    }
+
+    .card-btn:hover {
+        color: #0052ea;
+        text-decoration: none;
+        background-color: transparent;
+        border-color: transparent;
+    }
+
+    .plantel-row:hover {
+        background-color: #f8fafd;
+    }
+</style>
--- resources/js/administracion/planteles/views/Home.vue
+++ resources/js/administracion/planteles/views/Home.vue
@@ -66,11 +66,10 @@
                     <div class="card-header">
                         <h1 class="card-title">Centros de Trabajo</h1>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body bg-light">
                         <div class="row">
                             <div class="col">
-                                <p><strong>Planteles</strong>: {{planteles.pagination.total }}</p>
-
+                                <p class="mb-0"><strong>Planteles</strong>: {{planteles.pagination.total }}</p>
                             </div>
                             <div class="col">
                                 <div class="btn-group btn-group-sm pull-right" role="group" aria-label="Basic example">
@@ -79,52 +78,12 @@
                                     <button :disabled="planteles.pagination.nextPageUrl == null || loading" type="button" @click="next" class="btn btn-secondary">Siguiente
                                     </button>
                                 </div>
-                                <p><strong>Página</strong>: {{planteles.pagination.currentPage }} <strong>de</strong>: {{planteles.pagination.lastPage}}</p>
+                                <p class="mb-0"><strong>Página</strong>: {{planteles.pagination.currentPage }} <strong>de</strong>: {{planteles.pagination.lastPage}}</p>
                             </div>
                         </div>
-
                     </div>
-                    <div class="card-body p-0 table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Clave</th>
-                                    <th>Nombre</th>
-                                    <th>Municipio</th>
-                                    <th>Localidad</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <template v-if="loading">
-                                    <tr>
-                                        <td colspan="2">Cargando datos...</td>
-                                    </tr>
-                                </template>
-                                <template v-else>
-                                    <template v-for="plantel in planteles.items">
-                                        <tr>
-                                            <td>{{ plantel.clave}}</td>
-                                            <td>{{ plantel.nombre}}</td>
-                                            <td>{{ plantel.inmueble.municipio.NOM_MUN}}</td>
-                                            <td>{{ plantel.inmueble.localidad.NOM_LOC}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="4" class="border-0 pt-0">
-                                                <ul class="list-unstyled" style="font-size: 0.9rem">
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.control.descripcion}}</span></li>
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.sub_control.descripcion}}</span></li>
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.tipo_educativo.descripcion}}</span></li>
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.nivel_educativo.descripcion}}</span></li>
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.sub_nivel.descripcion}}</span></li>
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.tipo_centro_trabajo.descripcion}}</span></li>
-                                                    <li class="d-xs-table-row d-lg-table-cell pr-3"><span>{{ plantel.clasificador.descripcion}}</span></li>
-                                                </ul>
-                                            </td>
-                                        </tr>
-                                    </template>
-                                </template>
-                            </tbody>
-                        </table>
+                    <div class="card-body p-0">
+                        <row-item v-for="plantel in planteles.items" :key="plantel.id" :plantel="plantel"></row-item>
                     </div>
                 </div>
             </div>
@@ -134,9 +93,11 @@
 
 <script>
     import store from '../store/store';
+    import RowItem from '../components/RowItemComponent';
 
     export default {
         name: "Home",
+        components: {RowItem},
         data() {
             return {
                 filters: {
