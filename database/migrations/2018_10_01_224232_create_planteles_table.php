6ed2471a4d1a4870b8378a252cec53182a01356c
Andres Carrillo <andresjch2804@gmail.com>
2018-12-08 18:52:42 -0500

    Agrega tabla de subsistemas

--- database/migrations/2018_10_01_224232_create_planteles_table.php
+++ database/migrations/2018_10_01_224232_create_planteles_table.php
@@ -20,6 +20,7 @@ class CreatePlantelesTable extends Migration
                 ->comment('uuid del centro de trabajo en la tabla centros_trabajo de la base de datos de centrostrabajo');
             $table->string('descripcion');
             $table->string('clave', 10);
+
             $table->string('cve_ent', 2);
             $table->string('cve_mun', 3);
             $table->string('cve_loc', 4);
@@ -34,14 +35,15 @@ class CreatePlantelesTable extends Migration
             $table->integer('codigo_postal')->nullable();
             $table->decimal('latitud', 10, 8)->nullable();
             $table->decimal('longitud', 11, 8)->nullable();
-            $table->unsignedInteger('subsistema_id');
-            $table->foreign('subsistema_id')->references('id')->on('centrostrabajo.subsistemas');
-            $table->unsignedInteger('sede_ceneval')->nullable();
+            $table->unsignedTinyInteger('subsistema_id')->nullable();
+            $table->foreign('subsistema_id')->references('id')->on('subsistemas');
             $table->unsignedInteger('responsable_id')->nullable();
             $table->foreign('responsable_id')->references('id')->on('users');
+            $table->unsignedInteger('sede_ceneval')->nullable();
             $table->unsignedTinyInteger('descuento')->default(0);
             $table->string('pagina_web')->default('https://qroo.gob.mx/seq');
             $table->string('telefono', 30)->nullable();
+            $table->unsignedTinyInteger('opciones')->default(0);
             $table->boolean('active')->default(0);
 
             $table->index('uuid');
