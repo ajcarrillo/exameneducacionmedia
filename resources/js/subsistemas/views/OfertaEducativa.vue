e1a30a4555656936cb9e516aa8fbe48c73ed2953
Andres Carrillo <andresjch2804@gmail.com>
2019-01-14 23:07:54 -0500

    Elimina margin top

--- resources/js/subsistemas/views/OfertaEducativa.vue
+++ resources/js/subsistemas/views/OfertaEducativa.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <div class="container-fluid">
-            <div class="row mt-3">
+            <div class="row">
                 <div class="col">
                     <div class="card">
                         <div class="card-header d-flex align-items-center">
