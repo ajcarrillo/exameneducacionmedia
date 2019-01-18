efa02b3044ba4e66d9534dae28063b1ac4e1eac7
Igna Hernandez <sistemas.igna.hernandez@gmail.com>
2019-01-18 15:26:09 -0500

    Configurar rutas para el modulo de media superior

---
+++ resources/views/media_superior/administracion/etapas_proceso.blade.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igna
+ */
+?>
+
+@extends('layouts.app')
+
+@section('extra-head')
+@endsection
+
+@section('navbar-title')
+    Educación Media - Administración - Etapas del proceso
+@endsection
+
+@section('content')
+    <h1>hola</h1>
+@endsection
+
+@section('extra-scripts')
+@endsection
+
+
