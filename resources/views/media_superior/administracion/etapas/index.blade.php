f009808b5818a98625ca5dc31feea4548f1d990b
Igna Hernandez <sistemas.igna.hernandez@gmail.com>
2019-01-18 16:03:35 -0500

    Correccion en el nombre de la vista

--- resources/views/media_superior/administracion/etapas/index.blade.php
+++ resources/views/media_superior/administracion/etapas/index.blade.php
@@ -15,7 +15,44 @@
 @endsection
 
 @section('content')
-    <h1>hola</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @include('flash::message')
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <div class="card-title">Fechas</div>
+                        <div class="pull-right">
+                            <a class="btn btn-primary" title="Nuevo ciclo escolar"
+                               href="#">Nuevo</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive-sm">
+                            <table class="table table-bordered table-striped table-hover" id="ciclosEscolaresTable">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>---</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card-footer"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('extra-scripts')
