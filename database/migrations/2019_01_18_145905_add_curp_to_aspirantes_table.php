e0a8a28c065ebce1c73bb3ca4fbddaf66b52073f
Andres Carrillo <andresjch2804@gmail.com>
2019-01-21 23:47:01 -0500

    Agrega columnas faltantes a la tabla de aspirantes

---
+++ database/migrations/2019_01_18_145905_add_curp_to_aspirantes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCurpToAspirantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('aspirantes', function (Blueprint $table) {
+            $table->date('fecha_nacimiento')
+                ->after('informacion_procedencia')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('aspirantes', function (Blueprint $table) {
+            $table->dropColumn('fecha_nacimiento');
+        });
+    }
+}
