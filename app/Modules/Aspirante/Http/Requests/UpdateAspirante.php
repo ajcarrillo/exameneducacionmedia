a3f3090f78abf0b76918123bbf633edac4f385ef
Andres Carrillo <andresjch2804@gmail.com>
2019-01-23 20:53:48 -0500

    Crea endpoint para actualizar los datos generales de un aspirante

---
+++ app/Modules/Aspirante/Http/Requests/UpdateAspirante.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andres
+ * Date: 2019-01-23
+ * Time: 17:36
+ */
+
+namespace Aspirante\Http\Requests;
+
+
+use Aspirante\Rules\FechaNacimiento;
+use Aspirante\Traits\Utilities;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateAspirante extends FormRequest
+{
+    use Utilities;
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'nombre'                => 'required',
+            'primer_apellido'       => 'required',
+            'telefono'              => 'required',
+            'sexo'                  => 'required|in:H,M',
+            'pais_nacimiento_id'    => 'required',
+            'entidad_nacimiento_id' => 'required_if:pais_nacimiento_id,MX',
+            'fecha_nacimiento'      => [
+                'required',
+                new FechaNacimiento,
+            ],
+            'curp'                  => [
+                'required_if:pais_nacimiento_id,MX',
+                Rule::unique('aspirantes')->ignore($this->id),
+            ],
+            'curp_historica'        => 'required_with:curp',
+            'curp_valida'           => 'required_with:curp',
+        ];
+    }
+}
--- app/Modules/Aspirante/Routes/api.php
+++ app/Modules/Aspirante/Routes/api.php
@@ -4,4 +4,15 @@
  * User: andres
  * Date: 2019-01-15
  * Time: 23:01
+ *
+ * path: api/aspirantes
+ *
  */
+
+
+Route::middleware([ 'auth:api', 'role:aspirante' ])
+    ->name('aspirante.')
+    ->group(function () {
+        Route::patch('/{id}', 'API\UpdateAspiranteController@update')->name('update');
+    });
+
