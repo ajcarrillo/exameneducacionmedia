6b803ceb02af527796f5d804416907ff2b2af46c
Andres Carrillo <andresjch2804@gmail.com>
2019-01-27 14:36:04 -0500

    Agrega mensaje de exito o error al guardar los datos generales

--- resources/js/aspirante/components/App.vue
+++ resources/js/aspirante/components/App.vue
@@ -2,11 +2,20 @@
     <div class="">
         <section class="p-0 bg-light" id="intro">
             <div class="container">
-                <div class="row justify-content-center mb-5">
+                <div class="row justify-content-center py-5">
                     <div class="col col-md-10 col-lg-8">
                         <div class="row align-items-center">
-                            <div class="col-4 col-lg-3">x</div>
-                            <div class="col">x</div>
+                            <div class="col-4 col-lg-3">
+                                <avatar :gender="aspirante.sexo"></avatar>
+                            </div>
+                            <div class="col">
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <h2 class="mb-0"><b>{{ nombreCompleto }}</b></h2>
+                                        <span class="text-muted">Folio: {{ aspirante.folio }}</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -73,6 +82,7 @@
     import EditInformacionProcedencia from './informacion_procedencia/EditComponent';
     import CreateDomicilio from './domicilio/CreateComponent';
     import EditDomicilio from './domicilio/EditComponent';
+    import Avatar from '../../components/UserAvatarComponent';
 
     export default {
         name: "App",
@@ -81,7 +91,8 @@
             CreateInformacionProcedencia,
             EditInformacionProcedencia,
             CreateDomicilio,
-            EditDomicilio
+            EditDomicilio,
+            Avatar
         },
         props: ['asp', 'paises', 'municipios', 'entidades'],
         data() {
@@ -104,6 +115,12 @@
             },
             hasDomicilio() {
                 return this.aspirante.domicilio_id !== null;
+            },
+            usuario() {
+                return this.aspirante.user;
+            },
+            nombreCompleto() {
+                return `${this.usuario.nombre} ${this.usuario.primer_apellido} ${this.usuario.segundo_apellido === null ? '' : this.usuario.segundo_apellido}`;
             }
         },
         methods: {
