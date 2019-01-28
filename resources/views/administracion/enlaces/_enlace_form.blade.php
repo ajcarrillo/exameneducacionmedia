fd6ad3bc1a9192a023e237f1dcea731c22bf13ac
Andres Carrillo <andresjch2804@gmail.com>
2019-01-28 12:18:11 -0500

    Agrega fecha de inicio minima y fecha de fin maxima, segun las fechas de la etapa de registro

--- resources/views/administracion/enlaces/_enlace_form.blade.php
+++ resources/views/administracion/enlaces/_enlace_form.blade.php
@@ -6,13 +6,23 @@
 </div>
 <div class="form-group">
     {!! Form::label('fecha_inicio', 'Fecha inicio', ['class'=>'control-label']) !!}
-    {!! Form::date('fecha_inicio', NULL, ['class'=>'form-control '.($errors->has('fecha_inicio') ? ' is-invalid' : ''), 'required']) !!}
+    {!! Form::date('fecha_inicio', NULL, [
+    'class'=>'form-control '.($errors->has('fecha_inicio') ? ' is-invalid' : ''),
+    'min'=> $registro->apertura,
+    'max'=>$registro->cierre,
+    'required'
+    ]) !!}
     @errors(['errors'=>$errors, 'field'=>'fecha_inicio'])
     @enderrors
 </div>
 <div class="form-group">
     {!! Form::label('fecha_fin', 'Fecha fin', ['class'=>'control-label']) !!}
-    {!! Form::date('fecha_fin', NULL, ['class'=>'form-control '.($errors->has('fecha_fin') ? ' is-invalid' : ''), 'required']) !!}
+    {!! Form::date('fecha_fin', NULL, [
+    'class'=>'form-control '.($errors->has('fecha_fin') ? ' is-invalid' : ''),
+    'min'=> $registro->apertura,
+    'max'=>$registro->cierre,
+    'required'
+    ]) !!}
     @errors(['errors'=>$errors, 'field'=>'fecha_fin'])
     @enderrors
 </div>
@@ -40,6 +50,6 @@
 </div>
 <div class="form-group">
     {!! Form::label('extension', 'Extensión', ['class'=>'control-label']) !!}
-    {!! Form::text('extension', null, ['class'=>'form-control']) !!}
+    {!! Form::text('extension', NULL, ['class'=>'form-control']) !!}
 </div>
 <input type="hidden" name="cve_ent" value="23">
