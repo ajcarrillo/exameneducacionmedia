2f20b4e801b5703d819170ececc59e0ea12f7031
Miguel Protonotario <miguel.protonotario@seq.edu.mx>
2019-02-05 08:44:09 -0600

    correccion de visualizacion de mapas

--- resources/views/aspirante/dashboard.blade.php
+++ resources/views/aspirante/dashboard.blade.php
@@ -104,16 +104,7 @@
 
                                         </gmap-map>
 
-                                        <div class="card-body" style="height: 14cm">
-                                            <h2>
-                                                <span class="badge badge-pill badge-success">{{$oferta->preferencia}}</span>
-                                            </h2>
-                                            <div style="height: 140px;">
-                                                <p class="card-title text-primary">
-                                                    <b>{{ $oferta->ofertaEducativa->especialidad->referencia }}</b></p>
-                                                <p class="card-text">{{ $oferta->ofertaEducativa->plantel->descripcion }}</p>
-                                            </div>
-
+                                        
                                         <div class="card-body" style="height: 14cm">
                                             <h2><span class="badge badge-pill badge-success">{{$oferta->preferencia}}</span></h2>
                                             <div style="height: 140px;">
