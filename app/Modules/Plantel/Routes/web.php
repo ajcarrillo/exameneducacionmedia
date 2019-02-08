6da64704ad11251606af0c89ce2069254b64b8d3
SoancatlRosa <reita1991@gmail.com>
2019-02-08 12:06:05 -0500

    se borro un comentario en las rutas para module plantel

--- app/Modules/Plantel/Routes/web.php
+++ app/Modules/Plantel/Routes/web.php
@@ -1,19 +1,8 @@
 <?php
-
+//ruta-Rosa
 Route::middleware([ 'auth', 'role:plantel', 'hasPlantel' ])
     ->group(function () {
         Route::get('/', 'PanelController@index')->name('planteles.panel');
     });
 
-
-
-
-/*Route::get('/panel', 'PanelController@index')->name('panel');
-
-Route::middleware([ 'auth', 'role:plantel', 'hasPlantel' ])
-    ->prefix('/')
-    ->group(function () {
-        Route::get('/{all?}', 'PanelController')
-            ->where([ 'all' => '.*' ])
-            ->name('panel');
-    });*/
+//end ruta-Rosa
