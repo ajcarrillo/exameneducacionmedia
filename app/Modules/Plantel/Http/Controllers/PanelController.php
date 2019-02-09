d87d74e0d7081cc5c68b161286783f5ccbf41cc8
Andres Carrillo <andresjch2804@gmail.com>
2019-02-09 01:20:27 -0500

    Valida que no exista division entre cero

--- app/Modules/Plantel/Http/Controllers/PanelController.php
+++ app/Modules/Plantel/Http/Controllers/PanelController.php
@@ -12,49 +12,49 @@ class PanelController extends Controller
 {
     public function index()
     {
-        $usuario_id= Auth::user()->id;
+        $usuario_id = Auth::user()->id;
 
-        $var = Plantel::with("responsable")->where("responsable_id",$usuario_id)->get();
-        foreach ($var as $r){
-          $id_plantel= $r->id;
+        $var = Plantel::with("responsable")->where("responsable_id", $usuario_id)->get();
+        foreach ($var as $r) {
+            $id_plantel = $r->id;
         }
-        $total_oferta=OfertaEducativa::where('plantel_id', $id_plantel)->count();
-        $sqlof = 'SELECT COUNT( DISTINCT t.aspirante_id) as aspirante_id
+        $total_oferta = OfertaEducativa::where('plantel_id', $id_plantel)->count();
+        $sqlof        = 'SELECT COUNT( DISTINCT t.aspirante_id) as aspirante_id
                 FROM seleccion_ofertas_educativas as t
                 LEFT JOIN ofertas_educativas as oe on t.oferta_educativa_id = oe.id
-                WHERE oe.plantel_id ='.$id_plantel;
-        $respuestaof = DB::select($sqlof);
-        foreach ($respuestaof as $r){
+                WHERE oe.plantel_id =' . $id_plantel;
+        $respuestaof  = DB::select($sqlof);
+        foreach ($respuestaof as $r) {
             $total_demanda = $r->aspirante_id;
         }
 
-        $sqlpass= 'SELECT COUNT(DISTINCT(t.aspirante_id)) as aspirante_id
+        $sqlpass      = 'SELECT COUNT(DISTINCT(t.aspirante_id)) as aspirante_id
                 FROM pases_examen as t
                 LEFT JOIN seleccion_ofertas_educativas as sof on sof.aspirante_id = t.aspirante_id
                 LEFT JOIN ofertas_educativas as oe on oe.id = sof.oferta_educativa_id
-                WHERE oe.plantel_id ='.$id_plantel;
-        $respuestapas= DB::select($sqlpass);
-        foreach ($respuestapas as $rp){
-            $aspirantes_proceso_completo= $rp->aspirante_id;
+                WHERE oe.plantel_id =' . $id_plantel;
+        $respuestapas = DB::select($sqlpass);
+        foreach ($respuestapas as $rp) {
+            $aspirantes_proceso_completo = $rp->aspirante_id;
         }
 
-        $sqlp= 'SELECT COUNT(DISTINCT(t.aspirante_id)) as aspirante_id
+        $sqlp       = 'SELECT COUNT(DISTINCT(t.aspirante_id)) as aspirante_id
             FROM revision_registros as t
             LEFT JOIN seleccion_ofertas_educativas as sof on sof.aspirante_id = t.aspirante_id
             LEFT JOIN ofertas_educativas as oe on oe.id = sof.oferta_educativa_id
-            WHERE oe.plantel_id ='.$id_plantel.' and t.efectuado = 0';
-        $respuestap= DB::select($sqlp);
-        foreach ($respuestap as $rpp){
-            $aspirantes_sin_pago= $rpp->aspirante_id;
+            WHERE oe.plantel_id =' . $id_plantel . ' and t.efectuado = 0';
+        $respuestap = DB::select($sqlp);
+        foreach ($respuestap as $rpp) {
+            $aspirantes_sin_pago = $rpp->aspirante_id;
         }
 
-        $Aula = DB::table('aulas');
-        $aforo_suma= $Aula->where('edificio_id', $id_plantel)->sum('capacidad');
-        $aulas= $Aula->where('edificio_id', $id_plantel)->count('id');
+        $Aula       = DB::table('aulas');
+        $aforo_suma = $Aula->where('edificio_id', $id_plantel)->sum('capacidad');
+        $aulas      = $Aula->where('edificio_id', $id_plantel)->count('id');
 
-        $porcentaje = ($aspirantes_proceso_completo/$aforo_suma)*100;
+        $porcentaje = $aforo_suma = 0 ? ($aspirantes_proceso_completo / $aforo_suma) * 100 : 0;
 
-        $sede='SELECT DISTINCT (t.id) as id_sede ,t.descripcion as sede 
+        $sede  = 'SELECT DISTINCT (t.id) as id_sede ,t.descripcion as sede 
             , SUM(au.capacidad) as capacidad_aula
             , COUNT(DISTINCT au.id) as aulas,
             (SELECT COUNT(edificio_id) 
@@ -64,10 +64,10 @@ class PanelController extends Controller
             ) as capacidad_ocupada
             FROM sedes_alternas as t
             INNER JOIN aulas as au on au.edificio_id = t.id
-            WHERE t.plantel_id = '.$id_plantel.' AND au.edificio_type = "sede_alterna"
+            WHERE t.plantel_id = ' . $id_plantel . ' AND au.edificio_type = "sede_alterna"
             GROUP BY (t.id)';
-        $sedes= DB::select($sede);
+        $sedes = DB::select($sede);
 
-        return view('planteles.home', compact('total_oferta','total_demanda', 'aspirantes_proceso_completo', 'aspirantes_sin_pago','aforo_suma','aulas','porcentaje', 'sedes'));
+        return view('planteles.home', compact('total_oferta', 'total_demanda', 'aspirantes_proceso_completo', 'aspirantes_sin_pago', 'aforo_suma', 'aulas', 'porcentaje', 'sedes'));
     }
 }
