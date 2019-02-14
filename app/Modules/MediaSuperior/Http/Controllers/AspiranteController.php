90f4b56d4d5b118f9537fba78c3dce0293d72c6f
Andres Carrillo <andresjch2804@gmail.com>
2019-02-13 22:23:41 -0500

    Close #61, agrega funcionalidad para buscar aspirantes

---
+++ app/Modules/MediaSuperior/Http/Controllers/AspiranteController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andres
+ * Date: 2019-02-13
+ * Time: 21:08
+ */
+
+namespace MediaSuperior\Http\Controllers;
+
+
+use ExamenEducacionMedia\Http\Controllers\Controller;
+use ExamenEducacionMedia\User;
+use ExamenEducacionMedia\UserFilter;
+use Illuminate\Http\Request;
+
+class AspiranteController extends Controller
+{
+    public function index(Request $request, UserFilter $filters)
+    {
+        $users = User::query()
+            ->with('aspirante')
+            ->whereDoesntHave('roles', function ($query) {
+                $query->whereIn('name', [ 'supermario', 'cordinador', 'departamento',
+                    'subsistema', 'plantel', 'invitado', ]);
+            })
+            ->filterBy($filters, $request->only([ 'search', 'curp' ]))
+            ->paginate(50);
+
+        $users->appends($request->only([ 'curp', 'search' ]));
+
+        return view('administracion.aspirantes.index', compact('users'));
+    }
+}
---
+++ resources/views/administracion/aspirantes/index.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h1 class="card-title">
+                            Buscar aspirantes
+                        </h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="get"
+                              class="d-flex">
+                            <div class="pr-3 flex-fill">
+                                <input type="text"
+                                       class="form-control"
+                                       name="search"
+                                       placeholder="Ingresa nombre o email"
+                                       minlength="4"
+                                       value="{{ request('search') }}"
+                                       autofocus
+                                >
+                                <small class="text-muted">Mínimo 4 caracteres</small>
+                            </div>
+                            <div class="pr-3 flex-fill">
+                                <input type="text"
+                                       class="form-control"
+                                       name="curp"
+                                       placeholder="Ingresa folio o curp"
+                                       value="{{ request('curp') }}"
+                                       minlength="4"
+                                >
+                                <small class="text-muted">Mínimo 4 caracteres</small>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary">Buscar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-body p-0">
+                        @table
+                        @slot('thead')
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Folio</th>
+                                <th>Email</th>
+                                <th>CURP</th>
+                                <th>Opciones</th>
+                            </tr>
+                        @endslot
+                        @slot('tbody')
+                            @forelse($users as $user)
+                                <tr>
+                                    <td>
+                                        <a href="#!">
+                                            <i class="far fa-id-card pr-2"></i>
+                                            {{ get_full_name_from_user($user) }}
+                                        </a>
+                                    </td>
+                                    <td>
+                                        {{ $user->aspirante->folio }}
+                                    </td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>
+                                        {{ $user->aspirante->curp }}
+                                    </td>
+                                    <td>
+                                        @include('partials.login_as_user_form')
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="5" class="text-center">SIN RESULTADOS</td>
+                                </tr>
+                            @endforelse
+                        @endslot
+                        @endtable
+                    </div>
+                    <div class="card-footer">
+                        {{ $users->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
