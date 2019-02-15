239854cd871a5e34d4bd2c4679d7acc1f3417c8d
William Sanchez Fernandez <wsanchez7012@gmail.com>
2019-02-15 12:27:35 -0500

    Mezclar las operaciones de desactivar plantes y deactivar ofertas

--- resources/js/administracion/panel_departamento/home.js
+++ resources/js/administracion/panel_departamento/home.js
@@ -46,4 +46,66 @@ $(document).ready(function () {
         event.preventDefault();
 
     });
+
+    $('#btn_desactivar_planteles').click(function () {
+        $('#btn_desactivar_planteles').html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>')
+        swal.fire({
+            title: '¿Esta seguro de continuar?',
+            text: "Este proceso realizara la desactivación de los planteles",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Aceptar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax(
+                    {
+                        'url': '/administracion/panelAdministracion/desactivar-planteles',
+                        'type': 'get',
+                        'dataType': 'json',
+                    }
+                )
+                    .done(function (response) {
+                        if (response.code == 200)
+                            swal.fire(
+                                {
+                                    position: 'top-end',
+                                    type: 'success',
+                                    title: 'Los planteles se desactivaron satisfactoriamente',
+                                    showConfirmButton: false,
+                                    timer: 3000
+                                }
+                            )
+                        else
+                            swal.fire(
+                                {
+                                    position: 'top-end',
+                                    type: 'success',
+                                    title: 'Los planteles se no fueron desactivados',
+                                    showConfirmButton: false,
+                                    timer: 3000
+                                }
+                            )
+                    })
+                    .fail(function (xhr) {
+                        swal.fire(
+                            {
+                                position: 'top-end',
+                                type: 'success',
+                                title: 'Los planteles se no fueron desactivados',
+                                showConfirmButton: false,
+                                timer: 3000
+                            }
+                        )
+                        console.log(xhr);
+                    });
+            }
+        });
+
+        event.preventDefault();
+
+
+    });
 });
