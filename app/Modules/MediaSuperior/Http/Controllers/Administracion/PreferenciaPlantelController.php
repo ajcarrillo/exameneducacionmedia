8371ba8e0e4cd3ee316d53f9a1d52b90b2ee9963
Miguel Protonotario <mprotonotariodominguez@gmail.com>
2019-02-20 11:54:33 -0600

    correccion  de query [left join]

--- app/Modules/MediaSuperior/Http/Controllers/Administracion/PreferenciaPlantelController.php
+++ app/Modules/MediaSuperior/Http/Controllers/Administracion/PreferenciaPlantelController.php
@@ -22,7 +22,7 @@ class PreferenciaPlantelController
         $filtro = "";
 
         $datos = DB::table('seleccion_ofertas_educativas')
-                    ->select('aspirantes.id','aspirantes.folio', DB::raw("concat(users.nombre,' ',users.primer_apellido,' ',segundo_apellido) as nombre_completo"), 'especialidades.referencia as primera_opcion', 'subsistemas.referencia as subsistema', 'geo.NOM_MUN as municipio', DB::raw('IF(revision_registros.efectuado > 0, "Pagado", "Pendiente") as pago'), DB::raw('IF(pases_examen.id > 0, "Concluido", "Concluso") as concluyo_registo'))
+                    ->select('aspirantes.id','aspirantes.folio', DB::raw("concat(users.nombre,' ',users.primer_apellido,' ',segundo_apellido) as nombre_completo"), 'especialidades.referencia as primera_opcion', 'subsistemas.referencia as subsistema', 'geo.NOM_MUN as municipio', DB::raw('IF(revision_registros.efectuado = null, "Pagado", "Pendiente") as pago'), DB::raw('IF(pases_examen.id = null, "Concluido", "Concluso") as concluyo_registo'))
                     ->join('aspirantes','aspirantes.id','=','seleccion_ofertas_educativas.aspirante_id')
                     ->join('users','users.id','=','aspirantes.user_id')
                     ->join('ofertas_educativas','ofertas_educativas.id','=','seleccion_ofertas_educativas.oferta_educativa_id')
@@ -30,7 +30,7 @@ class PreferenciaPlantelController
                     ->join('planteles','planteles.id','=','ofertas_educativas.plantel_id')
                     ->join('subsistemas','subsistemas.id','=','planteles.subsistema_id')
                     ->join('geodatabase.mun_loc_qroo_camp as geo','geo.CVE_MUN','=','planteles.cve_mun')
-                    ->join('revision_registros','revision_registros.aspirante_id','=','aspirantes.id')
+                    ->leftjoin('revision_registros','revision_registros.aspirante_id','=','aspirantes.id')
                     ->leftjoin('pases_examen','pases_examen.aspirante_id','=','aspirantes.id')
                     ->where('seleccion_ofertas_educativas.preferencia',1)
 
@@ -88,7 +88,7 @@ class PreferenciaPlantelController
             ->setOption('zoom', '1');
 
         $datos = DB::table('seleccion_ofertas_educativas')
-            ->select('aspirantes.id','aspirantes.folio', DB::raw("concat(users.nombre,' ',users.primer_apellido,' ',segundo_apellido) as nombre_completo"), 'especialidades.referencia as primera_opcion', 'subsistemas.referencia as subsistema', 'geo.NOM_MUN as municipio', DB::raw('IF(revision_registros.efectuado > 0, "Pagado", "Pendiente") as pago'), DB::raw('IF(pases_examen.id > 0, "Concluido", "Concluso") as concluyo_registo'))
+            ->select('aspirantes.id','aspirantes.folio', DB::raw("concat(users.nombre,' ',users.primer_apellido,' ',segundo_apellido) as nombre_completo"), 'especialidades.referencia as primera_opcion', 'subsistemas.referencia as subsistema', 'geo.NOM_MUN as municipio', DB::raw('IF(revision_registros.efectuado = null, "Pagado", "Pendiente") as pago'), DB::raw('IF(pases_examen.id = null, "Concluido", "Concluso") as concluyo_registo'))
             ->join('aspirantes','aspirantes.id','=','seleccion_ofertas_educativas.aspirante_id')
             ->join('users','users.id','=','aspirantes.user_id')
             ->join('ofertas_educativas','ofertas_educativas.id','=','seleccion_ofertas_educativas.oferta_educativa_id')
@@ -96,7 +96,7 @@ class PreferenciaPlantelController
             ->join('planteles','planteles.id','=','ofertas_educativas.plantel_id')
             ->join('subsistemas','subsistemas.id','=','planteles.subsistema_id')
             ->join('geodatabase.mun_loc_qroo_camp as geo','geo.CVE_MUN','=','planteles.cve_mun')
-            ->join('revision_registros','revision_registros.aspirante_id','=','aspirantes.id')
+            ->leftjoin('revision_registros','revision_registros.aspirante_id','=','aspirantes.id')
             ->leftjoin('pases_examen','pases_examen.aspirante_id','=','aspirantes.id')
             ->where('seleccion_ofertas_educativas.preferencia',1)
 
