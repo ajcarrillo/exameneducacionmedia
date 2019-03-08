0cbb1550862d4c45db92044b8254c5b821b45b70
Andres Carrillo <andresjch2804@gmail.com>
2019-03-08 13:16:50 -0500

    Agrega transacción a la hora de crear el registro de un alumno externo

--- app/Modules/Aspirante/Http/Controllers/RegistroExternoController.php
+++ app/Modules/Aspirante/Http/Controllers/RegistroExternoController.php
@@ -11,6 +11,7 @@ namespace Aspirante\Http\Controllers;
 
 use Aspirante\Models\Aspirante;
 use Auth;
+use DB;
 use ExamenEducacionMedia\Http\Controllers\Controller;
 use ExamenEducacionMedia\Http\Requests\StoreUser;
 use ExamenEducacionMedia\Models\Geodatabase\Pais;
@@ -30,20 +31,25 @@ class RegistroExternoController extends Controller
     public function store(StoreUser $request)
     {
         $request->validated();
+        try {
+            DB::transaction(function () use ($request) {
+                $user = User::createUser($request->input(), [ 'aspirante' ]);
 
-        $user = User::createUser($request->input(), [ 'aspirante' ]);
+                $folio = Folio::getFolio();
 
-        $folio = Folio::getFolio();
+                $aspirante        = new Aspirante($request->only('fecha_nacimiento'));
+                $aspirante->folio = $folio->folio;
+                $folio->desactivar();
 
-        $aspirante        = new Aspirante($request->only('fecha_nacimiento'));
-        $aspirante->folio = $folio->folio;
-        $folio->desactivar();
+                $aspirante->user()->associate($user)->save();
 
-        $aspirante->user()->associate($user)->save();
+                $this->guard()->login($user);
+            });
 
-        $this->guard()->login($user);
-
-        return redirect('/aspirantes');
+            return redirect('/aspirantes');
+        } catch (\Throwable $e) {
+            abort(422, 'Lo sentimos ha ocurrido un error, intenta de nuevo');
+        }
     }
 
     protected function guard()
