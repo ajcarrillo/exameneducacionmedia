149c47b55eca6b13820925179882ae0f598f3117
Andres Carrillo <andresjch2804@gmail.com>
2019-03-11 13:45:44 -0500

    Agrega numeros a las opciones del menu de aspirantes

--- resources/views/aspirante/partials/call_actions_list.blade.php
+++ resources/views/aspirante/partials/call_actions_list.blade.php
@@ -1,21 +1,21 @@
 <ul class="list-unstyled call-to-action">
     <li>
         <a href="{{ route('aspirante.profile') }}"
-           class="btn bg-primary btn-sm">
-            Editar perfil
+           class="btn bg-primary btn-sm btn-block">
+            1 - Editar perfil
         </a>
     </li>
     <li>
         <a href="/aspirantes/captura-cuestionario"
-           class="btn btn-primary btn-sm">Cuestionario CENEVAL</a>
+           class="btn btn-primary btn-sm btn-block">2 - Cuestionario CENEVAL</a>
     </li>
     <li>
         <a href="/aspirantes/opciones-educativas"
-           class="btn btn-primary btn-sm">Opciones educativas</a>
+           class="btn btn-primary btn-sm btn-block">3 - Opciones educativas</a>
     </li>
 	<li>
 		<a href="{{ route('centro-descarga.cdescarga.index') }}"
-		   class="btn btn-primary btn-sm">Centro de descargas</a>
+		   class="btn btn-success btn-sm btn-block">Centro de descargas</a>
 	</li>
     @if($hasRevision)
         @if($revision->efectuado)
