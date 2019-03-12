a10b04c2aa7dc123aa3dc29bf47813c91117c58a
Andres Carrillo <andresjch2804@gmail.com>
2019-03-12 14:10:29 -0500

    Crea mensajes para traducir las claves de turno id y sexo

---
+++ resources/lang/es/sexo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andres
+ * Date: 2019-03-12
+ * Time: 13:50
+ */
+return [
+    'H' => 'HOMBRE',
+    'M' => 'MUJER',
+];
---
+++ resources/lang/es/turnos.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andres
+ * Date: 2019-03-12
+ * Time: 13:30
+ */
+return [
+    '1' => 'MATUTINO',
+    '2' => 'VESPERTINO',
+    '3' => 'NOCTURNO',
+    '4' => 'DISCONTINUO',
+    '5' => 'CONTINUO',
+];
