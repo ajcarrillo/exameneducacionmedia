04bc88e52e253211a3df647fc22245b4b38d5896
Andres Carrillo <andresjch2804@gmail.com>
2019-03-20 19:52:37 -0500

    Modifica el template del aviso de cuestionario

--- resources/views/aspirante/cuestionario/aviso_aspirante.blade.php
+++ resources/views/aspirante/cuestionario/aviso_aspirante.blade.php
@@ -5,28 +5,23 @@
  */
 ?>
 
-@extends('layouts.app')
-
-@section('extra-head')
-@endsection
-
-@section('navbar-title')
-	Aspirante - Capturar cuestionario ceneval
-@endsection
+@extends('aspirante.layouts.aspirante')
 
 @section('content')
-	<div class="container">
+    <div class="container mt-3">
+        <div class="row justify-content-center py-3">
+            <div class="col">
+                @include('aspirante.partials.back_button')
+            </div>
+        </div>
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="card card-primary card-outline">
 					<div class="card-header">
-						<div class="card-title">CAPTURAR CUESTIONARIO CENEVAL</div>
+                        <div class="card-title">CUESTIONARIO CENEVAL</div>
 					</div>
 					<div class="card-body">
 						<div class="alert alert-primary"> {{ $_REQUEST['aviso'] }} </div>
-					</div>
-					<div class="card-footer">
-						<a type="button" class="btn btn-light" href="{{ route('aspirante.dashboard.index') }}">Inicio</a>
 					</div>
 				</div>
 				{!! Form::close() !!}
