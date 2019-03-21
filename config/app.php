b0e04d687c1df7ff5b49f5ba44d6b435381c2099
Andres Carrillo <andresjch2804@gmail.com>
2019-03-21 12:35:41 -0500

    Elimina alias de carbon

--- config/app.php
+++ config/app.php
@@ -226,7 +226,6 @@ return [
         'Form'         => Collective\Html\FormFacade::class,
         'Html'         => Collective\Html\HtmlFacade::class,
         'Excel'        => Maatwebsite\Excel\Facades\Excel::class,
-        'Carbon'       => 'Carbon\Carbon',
     ],
 
 ];
