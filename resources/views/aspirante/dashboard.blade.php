478528d149ae8c61af8460d16b539f03e82b96c5
mprotonotario <miguel.protonotario@seq.edu.mx>
2019-04-04 11:55:58 -0500

    se elimino temporalmente el mapa gral de url: /aspirantes

--- resources/views/aspirante/dashboard.blade.php
+++ resources/views/aspirante/dashboard.blade.php
@@ -80,7 +80,7 @@
                         <div class="card">
 
 
-                            <div id="map" style="width: 100%; height: 400px; border: 1px solid grey" data-datos="{{$ofertas_gral}}" ></div>
+                            {{-- <div id="map" style="width: 100%; height: 400px; border: 1px solid grey" data-datos="{{$ofertas_gral}}" ></div>--}}
                             {{--<div class="row">
                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                                     <div class="card text-center">
@@ -106,7 +106,7 @@
                                 @foreach($ofertas as $oferta)
                                     <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex">
                                         <div class="card text-center align-self-stretch">
-                                            <iframe width="425" height="350" frameborder="0" scrolling="no"
+                                            <iframe width="100%" height="350" frameborder="0" scrolling="no"
                                                     marginheight="0" marginwidth="0"
                                                     src="https://www.openstreetmap.org/export/embed.html?bbox={{ $oferta->ofertaEducativa->plantel->longitud + .0009}}%2C{{ $oferta->ofertaEducativa->plantel->latitud + .0009}}%2C{{ $oferta->ofertaEducativa->plantel->longitud}}%2C{{ $oferta->ofertaEducativa->plantel->latitud}}&amp;layer=mapnik&amp;marker={{ $oferta->ofertaEducativa->plantel->latitud}}%2C{{ $oferta->ofertaEducativa->plantel->longitud}}"
                                                     ></iframe>
