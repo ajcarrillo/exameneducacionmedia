2466e41ec1675ad36db2dfa0d9c096c5c83d69ce
Andres Carrillo <andresjch2804@gmail.com>
2019-04-09 11:09:25 -0500

    Inicio de asignar pago

---
+++ app/Modules/Billy/Http/Controllers/AsignarPagoController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andres
+ * Date: 2019-04-08
+ * Time: 23:58
+ */
+
+namespace Billy\Http\Controllers;
+
+
+use ExamenEducacionMedia\Http\Controllers\Controller;
+
+class AsignarPagoController extends Controller
+{
+
+    public function store()
+    {
+        
+    }
+
+}
