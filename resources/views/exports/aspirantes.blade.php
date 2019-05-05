93ac214e23391e3a9dec8bc99032f2ea7f3ec6a1
Andres Carrillo <andresjch2804@gmail.com>
2019-05-05 17:46:36 -0500

    Agrega compos al reporte de aspirantes

--- resources/views/exports/aspirantes.blade.php
+++ resources/views/exports/aspirantes.blade.php
@@ -3,12 +3,22 @@
         <tr>
             <th>FOLIO</th>
             <th>NOMBRE</th>
+            <th>SEXO</th>
+            <th>FECHA_NACIMIENTO</th>
             <th>CURP</th>
+            <th>CURP</th>
+            <th>CURP_VALIDA</th>
+            <th>CURP_HISTORICA</th>
+            <th>MATRICULA</th>
             <th>EMAIL</th>
             <th>DOMICILIO</th>
+            <th>TELEFONO</th>
             <th>TIENE_PAGO</th>
             <th>ENVIO_REGISTRO</th>
             <th>TIENE_PASE</th>
+            <th>PROCEDENCIA_CLAVE_CENTRO_TRABAJO</th>
+            <th>PROCEDENCIA_NOMBRE_CENTRO_TRABAJO</th>
+            <th>FECHA_EGRESO</th>
             <th>PRIMERA_OPCION</th>
             <th>PRIMERA_OPCION_MUNICIPIO</th>
         </tr>
@@ -18,12 +28,22 @@
             <tr>
                 <td>{{ $aspirante->folio }}</td>
                 <td>{{ $aspirante->nombre_completo }}</td>
+                <td>{{ $aspirante->sexo }}</td>
+                <td>{{ $aspirante->fecha_nacimiento }}</td>
                 <td>{{ $aspirante->curp }}</td>
+                <td>{{ $aspirante->curp }}</td>
+                <td>{{ $aspirante->curp_valida }}</td>
+                <td>{{ $aspirante->curp_historica }}</td>
+                <td>{{ $aspirante->matricula }}</td>
                 <td>{{ $aspirante->email }}</td>
                 <td>{{ $aspirante->domicilio }}</td>
+                <td>{{ $aspirante->telefono }}</td>
                 <td>{{ $aspirante->tiene_pago }}</td>
                 <td>{{ $aspirante->envio_registro }}</td>
                 <td>{{ $aspirante->con_pase }}</td>
+                <td>{{ $aspirante->procedencia_clave_centro_trabajo }}</td>
+                <td>{{ $aspirante->procedencia_nombre_centro_trabajo }}</td>
+                <td>{{ $aspirante->fecha_egreso }}</td>
                 <td>{{ $aspirante->primera_opcion }}</td>
                 <td>{{ $aspirante->nombre_municipio }}</td>
             </tr>
